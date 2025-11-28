34258ad6da90eaf8accdb8f3f6bfa09b14ea9925
EuFlo <16579722+EuFlo@users.noreply.github.com>
2025-11-28 17:01:48 +0200

    Update charset.c

--- libs/libcommon/charset.c
+++ libs/libcommon/charset.c
@@ -32,21 +32,29 @@ char* charset_get_current(void)
 	return charset;
 }
 
-char* charset_convert(const char *string, size_t insize, const char *from, const char *to)
+//  Extended version of 'charset_convert' which has added an 'outsizebytes_ptr' parameter
+//  Converts a string with insize bytes from  'from' encodings to 'to' encondings
+//      instring is input string to be converted
+//      insizebytes is size of string in bytes without null-terminator !!!!
+//      outsizebytes_ptr contains the size in bytes of returned converted string (without null-terminator char)
+//      returns a pointer to the null-terminated converted string or, in case of an error, a clone of the same input instring
+//      returns NULL only if instring is NULL or calloc fails.
+//  User must free the returned buffer !!!!
+char* charset_convert_ext(const char *instring, size_t insizebytes,size_t *outsizebytes_ptr, const char *from, const char *to)
 {
 	size_t outbytesleft, outbytes_initial, inbytesleft, outsize, nconv;
 	iconv_t cd;
 	char *out, *outptr;
-	const char *input = string;
-	size_t used = 0U;
+	const char *input = instring;
+	size_t used = 0;
 	size_t null_char_width = 4U;
 	int retry_no = 0;
 
 	// sanity checks
-	if (!string)
+	if (instring == NULL)
 		return NULL;
-	if(insize > (size_t)32768U)
-		return NULL;
+	if(insizebytes > (size_t)32768U)
+		insizebytes =32768U;
 
 	if (!from)
 		from = charset_get_current();
@@ -65,12 +73,12 @@ char* charset_convert(const char *string, size_t insize, const char *from, const
 	/* + 4 for nul in case len == 1 */
 	/* For macOS Sonoma 14.4/15: makes more room because iconv behaves different than GNU and did not have errno == E2BIG !!! */
 
-	outsize = (insize + 1) * null_char_width;	
+	outsize = (insizebytes + 1) * null_char_width;	
 
-	out = malloc(outsize);
+	out = calloc(1,outsize);
 	if(out == NULL)
 	{
-		LOG(lm_main, LOG_ERROR, ("Error in charset_convert(): malloc() ret NULL!"));		
+		LOG(lm_main, LOG_ERROR, ("Error in charset_convert(): calloc() ret NULL!"));		
       	goto Error_case_ret;
 	}
 	
@@ -78,11 +86,7 @@ char* charset_convert(const char *string, size_t insize, const char *from, const
 	outbytesleft = outsize - null_char_width;
 	outbytes_initial = outbytesleft;
 	outptr = out;
-	inbytesleft = insize;
-
-//	LOG(lm_main, LOG_NOTICE, ("START charset_convert(%s -> %s): input=%p; inbytesleft=%zu, out=outptr=%p; outsize=%zu; outbytesleft=%zu",
-//	from, to, input,inbytesleft,out,outsize,outbytesleft));
-
+	inbytesleft = insizebytes;
 
  retry:
 #ifdef __lv2ppu__ 
@@ -95,9 +99,6 @@ char* charset_convert(const char *string, size_t insize, const char *from, const
 		switch (errno)
 		{
 			case E2BIG: /*(need more space - on macOS Sonoma 14.4/14.5 did not land here !!)*/
-			
-				// LOG(lm_main, LOG_NOTICE, ("charset_convert() -> case E2BIG; before realloc; input=%p; inbytesleft=%zu; outptr=%p; outbytesleft=%zu", 
-				// input,inbytesleft,outptr, outbytesleft));
 
 				used = 0;
 				if(outptr - out > 0)
@@ -118,16 +119,12 @@ char* charset_convert(const char *string, size_t insize, const char *from, const
 				outptr = out + used;
 				outbytesleft = outsize - used - null_char_width;
 				outbytes_initial = outbytesleft;
-				
-				// LOG(lm_main, LOG_NOTICE, ("charset_convert() -> case E2BIG; after realloc; input=%p; out=%p; outptr=%p; outsize=%zu; used=%zu; outbytesleft=%zu", 
-				// input,out,outptr,outsize,used, outbytesleft));
 
 				if(retry_no < 10)
 				{
 					retry_no ++;
 					goto retry;
 				}
-
 				else
 					break;
 			case EINVAL:
@@ -139,7 +136,7 @@ char* charset_convert(const char *string, size_t insize, const char *from, const
 				/* Invalid sequence, try to get the rest of the string */
 				LOG(lm_main, LOG_ERROR, ("charset_convert() -> case EILSEQ; (Invalid input sequence); "
 			  		"input=%p; outptr=%p; outsize=%zu", input,outptr,outsize));								   
-				if(input + 1 < string + insize)
+				if(input + 1 < instring + insizebytes)
 				{
 					input++;
 					inbytesleft--;
@@ -150,7 +147,7 @@ char* charset_convert(const char *string, size_t insize, const char *from, const
 			default:
 				LOG(lm_main, LOG_ERROR, ("charset_convert() ->  case Default. Conversion failed. Charsets: %s -> %s; Inputstring: %s; "
 					  "errno=%X;  Error: %s",
-					  from,to,string,errno, strerror(errno)));
+					  from,to,instring,errno, strerror(errno)));
 				goto Error_case;
 		}
 	}
@@ -161,41 +158,80 @@ char* charset_convert(const char *string, size_t insize, const char *from, const
 
 	iconv_close(cd);
 
-	// LOG(lm_main, LOG_NOTICE, ("END charset_convert() OK. nconv=%zu; input=%p; inbytesleft=%zu; out=%p; outptr=%p; outbytesleft=%zu; outbytes_initial=%zu",
-	// 		  		nconv,input,inbytesleft,out, outptr,outbytesleft,outbytes_initial));
+	if(outsizebytes_ptr != NULL)
+		*outsizebytes_ptr = outbytes_initial - outbytesleft;
 
 	return out;
 
 Error_case:
 	
 	iconv_close(cd);
-	if(out != NULL)free(out);
+	free(out);
 
 Error_case_ret:
-	LOG(lm_main, LOG_ERROR, ("charset_convert() ->  Error_case"));
-	
-	out = malloc(insize);
+	LOG(lm_main, LOG_ERROR, ("charset_convert() ->  Error_case_ret"));
+	// just return the clone of the input instring if calloc did not fail 
+	out = calloc(1,insizebytes + null_char_width); // add null terminator
 	if(out != NULL)
 	{
-		//memset(out, 0, insize);    
-		strncpy(out, string, insize);
+		memcpy(out,instring,insizebytes);
+		if(outsizebytes_ptr != NULL) *outsizebytes_ptr = insizebytes;
 	}
+	else
+		if(outsizebytes_ptr != NULL) *outsizebytes_ptr = 0;
 
 	return out;
+}
 
-	//return strdup(string);
+// converts a string with insize bytes from  'from' encodings to 'to' encondings
+//      instring is input string to be converted
+//      insize is size of string in bytes !!!!
+//      returns a pointer to the converted string or, 
+//                         in case of an error, a clone of the same input instring
+//      returns NULL only if instring is NULL or calloc fails.
+//  User must free the returned buffer !!!!
+char* charset_convert(const char *instring, size_t insizebytes, const char *from, const char *to)
+{
+
+	return charset_convert_ext(instring, insizebytes, NULL, from, to);
+}
+
+
+wchar_t* utf8char2wchar(const char *instring)
+{
+	wchar_t *out_wide_ptr = NULL;
+	size_t outsizebytes;
+	char *out_char_tmp;
+
+#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
+	out_char_tmp = charset_convert_ext(instring, strlen(instring), &outsizebytes, "UTF-8", "UCS-2-INTERNAL");
+#else
+	out_char_tmp = charset_convert_ext(instring, strlen(instring), &outsizebytes, "UTF-8", "WCHAR_T");
+#endif
+
+    if(out_char_tmp != NULL && outsizebytes > 0)
+	{
+		size_t nbr_of_wchar = outsizebytes / sizeof(wchar_t);
+		out_wide_ptr = calloc(nbr_of_wchar + 1, sizeof(wchar_t)); // +1 for null-terminator
+		memcpy(out_wide_ptr, out_char_tmp, outsizebytes);
+		free(out_char_tmp);
+
+		return out_wide_ptr;
+	}
+	else
+		return NULL;
 }
 
 char* charset_from_utf8(const char *string)
 {
 	if (!string)
 		return NULL;
-	return charset_convert(string, strlen(string), "UTF-8", NULL);
+	return charset_convert_ext(string, strlen(string), NULL, "UTF-8", NULL);
 }
 
 char* charset_to_utf8(const char *string)
 {
 	if (!string)
 		return NULL;
-	return charset_convert(string, strlen(string), NULL, "UTF-8");
+	return charset_convert_ext(string, strlen(string), NULL, NULL, "UTF-8");
 }
