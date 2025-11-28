0f3ca2731639571053490349d34475a62a534d0e
EuFlo <16579722+EuFlo@users.noreply.github.com>
2025-11-28 16:59:17 +0200

    Update charset.h

--- libs/libcommon/charset.h
+++ libs/libcommon/charset.h
@@ -8,17 +8,14 @@
 #ifndef CHARSET_H_INCLUDED
 #define CHARSET_H_INCLUDED
 
-#if defined(WIN32) || defined(_WIN32) || defined(WIN64) || defined(_WIN64)
-
-#define CHAR2WCHAR(dst, src) dst = (wchar_t *)charset_convert(src, strlen(src), "UTF-8", "UCS-2-INTERNAL")
-#else
-
-#define CHAR2WCHAR(dst, src) dst = (wchar_t *)charset_convert(src, strlen(src), "UTF-8", "WCHAR_T") 
-#endif
 
 char* charset_get_current(void);
+char* charset_convert_ext(const char *instring, size_t insizebytes,size_t *outsizebytes_ptr, const char *from, const char *to);
 char* charset_convert(const char *string, size_t insizebytes, const char *from, const char *to);
+wchar_t* utf8char2wchar(const char *instring);
 char* charset_to_utf8(const char *string);
 char* charset_from_utf8(const char *string);
+
+#define CHAR2WCHAR(dst, src) dst = utf8char2wchar(src)
 
 #endif  /* CHARSET_H_INCLUDED */
